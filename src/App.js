e826d1fd08e4cf4ef3026e83fa5ccc82999a5d06
Theerawat Kiatdarakun <tkiat@tutanota.com>
2021-02-20 14:26:20 +0700

    Follow tutorial from https://www.taniarascia.com/getting-started-with-react/

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Table from './Table.js'
+import Form from './Form.js'
+
+class App extends Component {
+  state = {
+    characters: [],
+  }
+
+  handleSubmit = (character) => {
+    this.setState({characters: [...this.state.characters, character]})
+  }
+
+  removeCharacter = (index) => {
+    const {characters} = this.state
+
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index
+      }),
+    })
+  }
+
+  render() {
+    const {characters} = this.state
+
+    return (
+      <div className="container">
+        <Table characterData={characters} removeCharacter={this.removeCharacter} />
+        <Form handleSubmit={this.handleSubmit} />
+      </div>
+    )
+  }
 }
-
-export default App;
+        // <h1>Hello, React!</h1>
+export default App
