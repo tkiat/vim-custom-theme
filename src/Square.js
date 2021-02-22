3418ffe78eb5da5383f4aad4839d721c975fc0b4
Theerawat Kiatdarakun <tkiat@tutanota.com>
2021-02-22 14:35:21 +0700

    UPDATE

--- src/Square.js
+++ src/Square.js
@@ -1,8 +1,6 @@
-import {Component} from 'react'
-
 const Square = (props) => {
   return (
-    <div className="square" style={{"background-color": props.backgroundcolor, "color": props.fontcolor}}>{props.value}</div>
+    <div className="square" style={{"backgroundColor": props.backgroundcolor, "color": props.fontcolor}}>{props.value}</div>
   )
 }
 export default Square
